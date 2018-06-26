df1bb457bee4cb42e71a442bca2afc665ea3fa55
Mattan <mattanserry@mail.tau.ac.il>
2018-06-26 16:07:56 +0300

    dataset work

--- model.py
+++ model.py
@@ -158,43 +158,78 @@ class Net(object):
             subnet.to(device=device)
 
 
+utkface_original_image_format = re.compile('^(\d+)_(\d+)_\d+_(\d+)\.jpg\.chip\.jpg$')
+
+MALE = 0
+FEMALE = 1
+
+from collections import namedtuple
+
+class Label(namedtuple('Label', ('age', 'gender'))):
+    def __init__(self, age, gender):
+        super(Label, self).__init__()
+        _age = self.age - 1
+        if _age < 20:
+            self.age_group = max(_age // 5, 0)  # first 4 age groups are for kids <= 20, 5 years intervals
+        else:
+            self.age_group = min(4 + (_age - 20) // 10, NUM_AGES - 1)  # last (6?) age groups are for adults > 20, 10 years intervals
+
+    def to_str(self):
+        return '%d_%d' % (self.age_group, self.gender)
+
+    def to_tensor(self):
+        age_tensor = -torch.ones(NUM_AGES)
+        age_tensor[self.age_group] *= -1
+        gender_tensor = -torch.ones(NUM_GENDERS)
+        gender_tensor[self.gender] *= -1
+        result = torch.cat((age_tensor, gender_tensor), 0)
+        result = result.to(device=device)
+        return result
+
+
 def sort_to_classes(root, print_cycle=np.inf):
     # Example UTKFace cropped and aligned image file format: [age]_[gender]_[race]_[date&time].jpg.chip.jpg
     # Should be 23613 images, use print_cycle >= 1000
-    utkface_original_image_format = re.compile('^(\d+)_\d+_\d+_(\d+)\.jpg\.chip\.jpg$')
-    files = iter([f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))])
+    # Make sure you have > 100 MB free space
+
+    def log(text):
+        print('[UTKFace dset labeler] ' + text)
+
+    log('Starting labeling process...')
+    files = [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f)) and utkface_original_image_format.match(f)]
+    if not files:
+        raise FileNotFoundError('No image files in '+root)
     copied_count = 0
-    sorted_folder = os.path.join(root, 'sorted')
+    sorted_folder = os.path.join(root, '..', 'labeled')
     if not os.path.isdir(sorted_folder):
         os.mkdir(sorted_folder)
 
     for f in files:
-        matcher = utkface_original_image_format.match(f)
-        if matcher is not None:
-            age, dtime = matcher.groups()
-            folder = os.path.join(sorted_folder, str(int(int(age) / 5)))
-            dst = os.path.join(folder, dtime+'.jpg')
-            if os.path.isfile(dst):
-                continue
-            if not os.path.isdir(folder):
-                os.mkdir(folder)
-            src = os.path.join(root, f)
-            copyfile(src, dst)
-            copied_count += 1
-            if copied_count % print_cycle == 0:
-                print('Copied %d files.' % copied_count)
-
+        srcfile = os.path.join(root, f)
+        age, gender, dtime = matcher.groups()
+        label = Label(int(age), int(gender))
+        dstfolder = os.path.join(sorted_folder, label.to_str())
+        dstfile = os.path.join(dstfolder, dtime+'.jpg')
+        if os.path.isfile(dstfile):
+            continue
+        if not os.path.isdir(dstfolder):
+            os.mkdir(dstfolder)
+        copyfile(srcfile, dstfile)
+        copied_count += 1
+        if copied_count % print_cycle == 0:
+            log('Copied %d files.' % copied_count)
+    log('Finished labeling process.')
 
 def get_utkface_dataset(root):
-    ret = lambda: torchvision.datasets.ImageFolder(os.path.join(root, 'sorted'), transform=transforms.ToTensor())
+    ret = lambda: torchvision.datasets.ImageFolder(os.path.join(root, 'labeled'), transform=transforms.ToTensor())
     try:
         return ret()
     except (RuntimeError, FileNotFoundError):
-        sort_to_classes(root, print_cycle=1000)
+        sort_to_classes(os.path.join(root, 'unlabeled'), print_cycle=1000)
         return ret()
 
 
-train_dataset = get_utkface_dataset('./data/UTKFace')
+train_dataset = get_utkface_dataset(os.path.join('.', 'data', 'UTKFace'))
 batch_size = 100
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
@@ -212,5 +247,5 @@ z = net.E(MOCK_IMAGE)
 dz = net.Dz(z)
 output = net.G(z, MOCK_AGES, MOCK_GENDERS)
 
-print(output)
+print(output.size())
 print(device)
